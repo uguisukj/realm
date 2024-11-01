8a0e731fa907e44f435be12b0152c55d38f6f8fc
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:33:14 -0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getAllUsers, getUserByUsername } = require('../controllers/userController');
+
+// Rota para obter todos os usuários
+router.get('/', getAllUsers);
+
+// Rota para obter um usuário específico
+router.get('/:username', getUserByUsername);
+
+module.exports = router;
 
