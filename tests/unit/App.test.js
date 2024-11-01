7118ccc83fd85c8aba08ca27527a936c10cff648
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:35:15 -0300

    Update App.test.js

--- tests/unit/App.test.js
+++ tests/unit/App.test.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../../src/App';
+
+test('renders welcome message', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Welcome to Realm/i);
+    expect(linkElement).toBeInTheDocument();
+});
 
