8dac849377873182d8eeab6eb0893a73516179ed
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:24:36 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem-vindo ao Realm!</h1>
+            <p>A sua comunidade aguarda por você.</p>
+        </div>
+    );
+};
+
+export default Home;
 
