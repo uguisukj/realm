d8559ac4c893bdc3b72ca8d38028a7a04623d92e
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:33:31 -0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+
+// Rota para registrar um novo usuário
+router.post('/register', register);
+
+// Rota para login
+router.post('/login', login);
+
+module.exports = router;
 
