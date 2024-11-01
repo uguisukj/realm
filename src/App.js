cd893f62eaa76250354ee7c98ccebe80674eef22
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:29:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import './styles/App.css';
+
+function App() {
+    return (
+        <Router>
+            <div className="container">
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/dashboard" component={Dashboard} />
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
 
