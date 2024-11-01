17bb5a17c65aee471e6e27f411be62b98519cbe4
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:32:23 -0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,10 @@
+// Modelo de usuário - simulação
+class User {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+module.exports = User;
 
