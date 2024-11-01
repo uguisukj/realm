a17c8fc74d8ff0a2a18799d664d3597f0105213d
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:32:39 -0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,17 @@
+let users = [];
+
+// Lógica para obter todos os usuários
+exports.getAllUsers = (req, res) => {
+    res.json(users);
+};
+
+// Lógica para obter um usuário específico
+exports.getUserByUsername = (req, res) => {
+    const user = users.find(u => u.username === req.params.username);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+};
 
