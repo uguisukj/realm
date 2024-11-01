c6cbbac219f768f7d1414c85267aabc4d75d5f07
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:25:23 -0300

    Update Dashboard.js

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>Painel de Controle</h2>
+            <p>Bem-vindo ao seu painel, aqui você pode gerenciar suas configurações e informações.</p>
+        </div>
+    );
+};
+
+export default Dashboard;
 
