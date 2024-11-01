c7a5939b1fef144681def559294e8264801eb39b
uguisukj <166897940+uguisukj@users.noreply.github.com>
2024-10-31 21:33:46 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
